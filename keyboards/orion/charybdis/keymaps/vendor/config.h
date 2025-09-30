8b2cbd7c5a15f316f0b6c7819eb506cbd078abaf
orion23rigel <orion.schmidt@gmail.com>
2025-09-29 20:11:35 -0400

    TD fix

--- keyboards/orion/charybdis/keymaps/vendor/config.h
+++ keyboards/orion/charybdis/keymaps/vendor/config.h
@@ -16,8 +16,8 @@
  */
 #pragma once
 
-//#define TAPPING_TERM 175
-//#define TAPPING_TERM_PER_KEY
+#define TAPPING_TERM 175
+#define TAPPING_TERM_PER_KEY
 
 #ifdef VIA_ENABLE
 /* VIA configuration. */
